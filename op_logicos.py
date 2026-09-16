6523acb43a31d4dd2c16d44aaa6a930d4f2c5f17
Laboratório FIAP <labsfiap@fiap.com.br>
2026-09-15 22:48:52 -0300

    operacoes com exercicios

--- op_logicos.py
+++ op_logicos.py
@@ -1,4 +1,28 @@
-import system
+import os
 os.system ("cls")
+
+#
+print(not True)
+print(not False)
+
+
+#and 'e'
+print(True and False)
+
+
+#or ou
+
+print(True or False)
+
+
+
+os.system ("cls")
+
+senha = 1234
+login = "eds"
+print(login == "edson" and senha == 1234)
+print(login == "edson" and senha == 1234)
+print(login == "edson" and senha == 1234)
+print(login == "edson" and senha == 1234)
 
 
---
+++ op_logicos_ex.py
@@ -0,0 +1,8 @@
+import os
+os.system ("cls")
+
+nota_aluno = int(input("Qual a nota do aluno"))
+
+nota = int(0,1,2,3,4,5,6,7,8,9,10)
+
+print( nota == nota_aluno)
